c346f9e30098fb404c01ffcc347d1a073801f6b6
Admin <pavel.planer@gmail.com>
2022-06-15 13:48:36 +0300

    1

--- untitled1/src/main/java/quoter/TerminatorQuoter.java
+++ untitled1/src/main/java/quoter/TerminatorQuoter.java
@@ -2,6 +2,7 @@ package quoter;
 
 import javax.annotation.PostConstruct;
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
--- untitled1/src/main/java/quoter/TestSpring.java
+++ untitled1/src/main/java/quoter/TestSpring.java
@@ -6,6 +6,7 @@ public class TestSpring {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
+        context.getBean(Quoter.class).sayQuote();
 
     }
 }
